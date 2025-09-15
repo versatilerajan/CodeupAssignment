9af2ceff0ef330d3e0b940a28ceffdf785b1486a
Rajan Singh ✅ <145154389+versatilerajan@users.noreply.github.com>
2025-09-15 15:02:57 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class table {
+public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number to print its multiplication table: ");
@@ -11,3 +11,4 @@ public class table {
         sc.close();
     }
 }
+
